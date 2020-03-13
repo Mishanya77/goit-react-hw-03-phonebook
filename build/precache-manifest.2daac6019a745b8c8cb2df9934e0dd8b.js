e7fac8b6a0ddb083701e20eaf1f3c87f763bfb05
Misha <gorban77@gmail.com>
2020-03-13 13:46:50 +0200

    hw3-1
    final ver

---
+++ build/precache-manifest.2daac6019a745b8c8cb2df9934e0dd8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7d6d8bdf2a04edb203c4709ca4b613",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "9e1d2729d72c382ae17f",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.0e226269.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.0e226269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2890b07f921a195d6dcb",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.eb9182ff.chunk.js"
+  },
+  {
+    "revision": "238a1528aed3a14ce705",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d660e622.js"
+  }
+]);
